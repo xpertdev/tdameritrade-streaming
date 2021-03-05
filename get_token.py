425a8803f7107eff89c11658421a8ceb42747add
Kiran <46508500+xpertdev@users.noreply.github.com>
2021-03-04 19:49:20 -0500

    Add support for Level2, Level1 and time of sale. Write quotes to disk. Support chrome beta.

--- get_token.py
+++ get_token.py
@@ -7,6 +7,9 @@ try:
     c = auth.client_from_token_file(config.TOKEN_PATH, config.API_KEY)
 except FileNotFoundError:
     from selenium import webdriver
-    with webdriver.Chrome(executable_path=config.CHROMEDRIVER_PATH) as driver:
+    chrome_options = webdriver.ChromeOptions()
+    if config.CHROME_BINARY:
+        chrome_options.binary_location = config.CHROME_BINARY
+    with webdriver.Chrome(executable_path=config.CHROMEDRIVER_PATH, options=chrome_options) as driver:
         c = auth.client_from_login_flow(
             driver, config.API_KEY, config.REDIRECT_URI, config.TOKEN_PATH)
--- stream.py
+++ stream.py
@@ -5,6 +5,8 @@ from tda.streaming import StreamClient
 import asyncio
 import json
 import config
+import os
+import file_helper as fileHelper
 
 client = easy_client(
         api_key=config.API_KEY,
@@ -12,17 +14,43 @@ client = easy_client(
         token_path=config.TOKEN_PATH)
 stream_client = StreamClient(client, account_id=config.ACCOUNT_ID)
 
-def order_book_handler(msg):
-    print(json.dumps(msg, indent=4))
+def level1_order_book_handler(msg):
+    write_to_file(msg, fileHelper.FileType.Level1)
+
+def nasdaq_order_book_handler(msg):
+    write_to_file(msg, fileHelper.FileType.Level2)
+
+def timesale_order_book_handler(msg)    :
+    write_to_file(msg, fileHelper.FileType.TimeSale)
+
+def write_to_file(msg, fileType):
+    fileType = fileHelper.FileType(fileType)
+    file_name = fileHelper.get_filename(fileType)
+    folder_name = os.path.join(config.QUOTE_PATH, fileType.name)
+    file_path = os.path.join(folder_name, file_name)
+
+    fileHelper.create_folder(folder_name)
+    fileHelper.write_file(msg, file_path)
+    print(file_name)
 
 async def read_stream():
     await stream_client.login()
     await stream_client.quality_of_service(StreamClient.QOSLevel.DELAYED)
-    await stream_client.nasdaq_book_subs(['GOOG'])
+    
+    #register level1 handler
+    stream_client.add_level_one_equity_handler(level1_order_book_handler)
+    await stream_client.level_one_equity_subs([config.SYMBOLS])
 
-    stream_client.add_nasdaq_book_handler(order_book_handler)
+    #register level2 handler
+    stream_client.add_nasdaq_book_handler(nasdaq_order_book_handler)
+    await stream_client.nasdaq_book_subs([config.SYMBOLS])
+
+    #register time of sale handler
+    stream_client.add_timesale_equity_handler(timesale_order_book_handler)
+    await stream_client.timesale_equity_subs([config.SYMBOLS])
 
     while True:
         await stream_client.handle_message()
 
-asyncio.get_event_loop().run_until_complete(read_stream())
+asyncio.run(read_stream())
+#asyncio.get_event_loop().run_until_complete(read_stream())
