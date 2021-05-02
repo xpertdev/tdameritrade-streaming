0f1cd1e6e77a06dfc1f6caa0c82a0fc7b075bf2c
Kiran <46508500+xpertdev@users.noreply.github.com>
2021-05-02 14:54:54 -0400

    refactor datalake code

--- datalake_helper.py
+++ datalake_helper.py
@@ -9,15 +9,6 @@ storage_account_name = config.STORAGE_ACCOUNT_NAME
 storage_account_key = config.STORAGE_ACCOUNT_KEY
 storage_filesystem_name = config.STORAGE_FILESYSTEM_NAME
 
-# set up the service client with the credentials from the environment variables
-service_client = DataLakeServiceClient(account_url="{}://{}.dfs.core.windows.net".format(
-    "https",
-    storage_account_name
-), credential=storage_account_key)
-
-# get the filesystem
-file_system_client = service_client.get_file_system_client(file_system=storage_filesystem_name)
-
 class FileType(Enum):
     Level1 = 1
     Level2 = 2
@@ -30,6 +21,15 @@ def get_filename(fileType):
 
 def write_file(msg, folder_name, file_name):
     
+    # set up the service client with the credentials from the environment variables
+    service_client = DataLakeServiceClient(account_url="{}://{}.dfs.core.windows.net".format(
+        "https",
+        storage_account_name
+    ), credential=storage_account_key)
+
+    # get the filesystem
+    file_system_client = service_client.get_file_system_client(file_system=storage_filesystem_name)
+
     directory_client = file_system_client.get_directory_client(folder_name)
     if not directory_client.exists():
         directory_client.create_directory()
